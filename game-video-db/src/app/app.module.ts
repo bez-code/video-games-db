e0e4bf6fafcee9899248052e62bcd45c554555ec
bez <Behzad.p.ashrafi@gmail.com>
2023-09-11 21:39:07 +0300

    Serach Component

---
+++ game-video-db/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { GaugeModule } from 'angular-gauge';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    GaugeModule.forRoot(),
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatTabsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
