351d5e7d1beaca1a380844e71e2650e057e3eb45
bez <Behzad.p.ashrafi@gmail.com>
2023-09-13 21:23:07 +0300

    Interceptors

--- game-video-db/src/app/app.module.ts
+++ game-video-db/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { GaugeModule } from 'angular-gauge';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { SearchBarComponent } from './component/search-bar/search-bar.component';
 import { HomeComponent } from './component/home/home.component';
+import { HttpErrorsInterceptors } from './interceptors/http-errors-interceptors';
+import { HttpHeadersInterceptors } from './interceptors/http-headers-interceptors';
 
 
 @NgModule({
@@ -33,7 +35,14 @@ import { HomeComponent } from './component/home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: HttpHeadersInterceptors, multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS, useClass: HttpErrorsInterceptors, multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
